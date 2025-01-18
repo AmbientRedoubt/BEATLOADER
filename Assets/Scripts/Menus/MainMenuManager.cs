3fa4467970f4f7dd4697cf462726cb1e315725ba
AmbientRedoubt <48478871+AmbientRedoubt@users.noreply.github.com>
2025-01-19 05:14:43 +1100

    First implementation of VHS post processing stack on MainMenu

--- Assets/Scripts/Menus/MainMenuManager.cs
+++ Assets/Scripts/Menus/MainMenuManager.cs
@@ -1,13 +1,8 @@
 using System;
 using FMODUnity;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class MainMenuManager : MonoBehaviour {
-    [SerializeField] private UIDocument _document;
-    private Button _startButton;
-    private Button _settingsButton;
-    private Button _quitButton;
     [field: SerializeField] private EventReference _clickSound;
 
     private void Awake() {
@@ -15,27 +10,18 @@ public class MainMenuManager : MonoBehaviour {
         GameManager.UpdateGameState(GameState.MainMenu);
     }
 
-    private void OnEnable() {
-        _startButton = _document.rootVisualElement.Q("StartButton") as Button;
-        _startButton.clicked += OnStartButtonClicked;
-        _settingsButton = _document.rootVisualElement.Q("SettingsButton") as Button;
-        _settingsButton.clicked += OnSettingsButtonClicked;
-        _quitButton = _document.rootVisualElement.Q("QuitButton") as Button;
-        _quitButton.clicked += OnQuitButtonClicked;
-    }
-
-    private void OnStartButtonClicked() {
+    public void OnStartButtonClicked() {
         AudioManager.PlayOneShot(_clickSound);
         GameManager.UpdateGameState(GameState.Playing);
         UnityEngine.SceneManagement.SceneManager.LoadScene("TestLevel");
     }
 
-    private void OnSettingsButtonClicked() {
+    public void OnSettingsButtonClicked() {
         AudioManager.PlayOneShot(_clickSound);
         throw new NotImplementedException();
     }
 
-    private void OnQuitButtonClicked() {
+    public void OnQuitButtonClicked() {
         AudioManager.PlayOneShot(_clickSound);
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;

b6f8b3d8188719c45c656fb73674402af1736a6e
AmbientRedoubt <48478871+AmbientRedoubt@users.noreply.github.com>
2025-01-20 03:17:35 +1100

    Added MilkShake camera shake, grouped 3rd party packages

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,9 +1,12 @@
+using MilkShake;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerController : MonoBehaviour {
     [SerializeField] private Canvas _canvas;
+    [SerializeField] private ShakePreset _crashShake;
+    [SerializeField] private ShakePreset _jumpShake;
     public static PlayerController Instance { get; private set; }
 
     private void Awake() {
@@ -29,6 +32,19 @@ public class PlayerController : MonoBehaviour {
 
     private void Update() {
 
+    }
+
+
+
+    private void OnJump() {
+        // AudioManager.PlayOneShot(Instance._jumpSound);
+        Shaker.ShakeAll(_jumpShake);
+    }
+
+    private void OnAttack() {
+        // Likely we won't need this action but keeping for testing purposes
+        // AudioManager.PlayOneShot(Instance._attackSound);
+        Shaker.ShakeAll(_crashShake);
     }
 
     private void OnEscape() {
